184a689b87ec323f684805ab71278728aad9d2eb
Rahammetoela <fuseteam@duck.com>
2024-10-25 15:15:52 -0300

    set the correct location for the company

--- assets/js/sss-street.js
+++ assets/js/sss-street.js
@@ -1,6 +1,8 @@
-var latitude = 5.809320;
-var longitude = -55.206474;
-var map = L.map('map', { zoomControl: false } ).setView([ latitude, longitude], 17);
+var clatitude = 5.809320;
+var clongitude = -55.206474;
+var latitude = 5.807153;
+var longitude = -55.205392;
+var map = L.map('map', { zoomControl: false } ).setView([ clatitude, clongitude], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
